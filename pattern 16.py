ece6f1ad2ab3d8381d3b69123ae1846d25546092
Himaja810 <167081819+Himaja810@users.noreply.github.com>
2024-04-29 09:39:46 +0530

    Create pattern 16.py

---
+++ pattern 16.py
@@ -0,0 +1,9 @@
+#pattern 16
+n=int(input())
+for r in range(1,n+1):
+    for s in range(r):
+        if r%2==0 :
+            print("@",end="")
+        else :
+            print(r,end="")
+    print()
